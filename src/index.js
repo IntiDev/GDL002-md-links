a1c38796dc11329482add34b441a725e6f767581
IntiDev <yeseniase@outlook.com>
2019-04-16 10:40:07 -0500

    Se limpia código siguiendo reglas de eslint

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ const fs = require('fs');
 
 const fileExists = fs.existsSync;
 
-const validateFormat = (file) =>  {
+const validateFormat = (file) => {
   let getExtensionFile = (/[.]/.exec(file)) ? /[^.]+$/.exec(file)[0] : undefined;
 
-  return getExtensionFile == 'md' ? true : false;
+  return getExtensionFile === 'md' ? true : false;
 };
 
 //    /\[(.?)\]\(.*?\)/gm
 // /^\[.*\]\(.*\)/
-//https://stackoverflow.com/questions/14599071/how-can-i-write-a-javascript-regular-expression-to-replace-hyperlinks-in-this-fo
-const readFile = (file, 'utf8') => {
+// https://stackoverflow.com/questions/14599071/how-can-i-write-a-javascript-regular-expression-to-replace-hyperlinks-in-this-fo
+// const readFile = (file) => {
+//   fs.readFileSync(file, 'utf8');
   
-  
-}
+// }
 
 module.exports = {
   fileExists,
   validateFormat,
-  readFile,
+  // readFile,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,31 +1,30 @@
 const fs = require('fs');
-const {fileExists, validateFormat, readFile} = require('../src/index.js');
+const {fileExists, validateFormat} = require('../src/index.js');
 
 describe('File exists', () => {
   it('will be return true when a file exists', () => {
     expect(fileExists('README.md')).toBe(true);
   });
 
-  it('will be return false when a file doesnt exists', () => {
+  it('will be return false when a file doesn\'t exists', () => {
     expect(fileExists('test.txt')).toBe(false);
   });
 });
 
 describe('Validate format', () => {
-
   it('is a function', () => {    
     expect(typeof validateFormat).toBe('function');
   });
 
-  it('is a markdown file ',() => {
+  it('is a markdown file ', () => {
     expect(validateFormat('README.md')).toBe(true);
-  } );
+  });
 
-  it('isnt a markdown file', () => {
+  it('isn\'t a markdown file', () => {
     expect(validateFormat('test.txt')).toBe(false);
   });
 });
 
 describe('Read file', () => {
-  
+
 });
